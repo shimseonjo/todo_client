d3844ed467f87cec6dbd4e709f4609bf52b5e8dd
shimseonjo <shimseonjo@gmail.com>
2024-04-02 11:11:09 +0900

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,27 +11,28 @@ export default function Home(){
   useEffect(() => {
     async function getData(){
       inputRef.current.focus()
-      const result = await axios.get('http://localhost:3001/get')
+      // const result = await axios.get('http://localhost:3001/get')
+      const result = await axios.get('https://todo-devpia.koyeb.app/get')
       setTodos(result.data)
     }
     getData()
   },[])
 
   const handleAdd = async()=> {
-    await axios.post('http://localhost:3001/add',{task:task})
-    const result = await axios.get('http://localhost:3001/get')
+    await axios.post('https://todo-devpia.koyeb.app/add',{task:task})
+    const result = await axios.get('https://todo-devpia.koyeb.app/get')
     setTodos(result.data)
     inputRef.current.value=""
     inputRef.current.focus()
   }
   const handleEdit = async(id)=> {
-   await axios.put('http://localhost:3001/update/'+id)
-   const result = await axios.get('http://localhost:3001/get')
+   await axios.put('https://todo-devpia.koyeb.app/update/'+id)
+   const result = await axios.get('https://todo-devpia.koyeb.app/get')
    setTodos(result.data)
   }
   const handleDelete = async(id)=> {
-    await axios.delete('http://localhost:3001/delete/'+id)
-    const result = await axios.get('http://localhost:3001/get')
+    await axios.delete('https://todo-devpia.koyeb.app/delete/'+id)
+    const result = await axios.get('https://todo-devpia.koyeb.app/get')
     setTodos(result.data)
   }
 
